e7466907ff8a6e17b7c0c37eb4668de7a50f916e
John M. Nichol <jmnichol@gmail.com>
2022-04-03 07:04:37 -0400

    Update casinoPSF2.m

--- casinoPSF2.m
+++ casinoPSF2.m
@@ -1,4 +1,4 @@
-function [psf] = casinoPSF2(  )
+function [psf] = casinoPSF2(config)
 %casinoPSF2 extracts a point spread function from a Casino dataset. 
 %
 %   Extracts a point spread function from a dataset generated using Casino.
@@ -12,71 +12,93 @@ function [psf] = casinoPSF2(  )
 %   a function of position. You should simulate something like 2,000-10,000
 %   electrons. The dat file will be about 150-600 MB.
 
+if ~exist('config','var')
+    config=struct();
+end
 
-[filename pathname]=uigetfile('*.dat');
-fID=fopen([pathname filename]);
+config=def(config,'beta0',[1 4  1.1 0 .1]); %parameters are [log10(alpha), log10(beta), log10(gamma), total backscattered ratio, nu]
+config=def(config,'file',[]); 
 
-% Find the energy deposited in the pmma vs lateral position.
-fprintf('Counting electrons\n');
+[filename pathname]=uigetfile({'*.dat';'*.mat'});
+[~,~,ext] = fileparts(fullfile(pathname,filename));
+
+%analyze a new simulation
+if strmatch(ext,'.dat')
+    fID=fopen([pathname filename]);
+    count_elec=1;
+end
+
+%in case we want to reanalyze data.
+if strmatch(ext,'.mat')
+    count_elec=0;
+    d=load([pathname filename]);
+    rvals=d.psf.rvals;
+    evals=d.psf.evals;
+end
+
 dr=1; %spacing in nanometers for the simulation
-rvals=(1:dr:50000);
-evals=rvals.*0;
-nvals=rvals.*0;
-count=0; %counts the number of collisions in the pmma that were included in the loop.
-xv=[];
-yv=[];
-maxE=0;
-lastLine=[];
-
-cont=1;
-tic
-while cont
-    A=fgetl(fID);
+if count_elec
+    % Find the energy deposited in the pmma vs lateral position.
+    fprintf('Counting electrons\n');
+    rvals=(1:dr:50000);
+    evals=rvals.*0;
+    nvals=rvals.*0;
+    count=0; %counts the number of collisions in the pmma that were included in the loop.
+    xv=[];
+    yv=[];
+    maxE=0;
+    lastLine=[];
     
-    if A==-1 
-        cont=0;
-        break
-    end
-    
-    skip=isempty(A);
-    if skip
-        continue
-    end
-    
-    [aa,n]=sscanf(A,'%f %f %f %f %f %f %f %s');
-    
-    if n==8
-        try
-            de=lastLine(7)-aa(7);
-        catch
-            de=0;
+    cont=1;
+    tic
+    while cont
+        A=fgetl(fID);
+        
+        if A==-1
+            cont=0;
+            break
         end
         
-        if ~isempty(regexp(A,'MMA')) %Only look at collisions in PMMA or MMA
-            xv=[xv aa(1)];
-            yv=[yv aa(2)]; 
-            r=(aa(1)^2+aa(2)^2)^(0.5);
-            ind=round(r/dr)+1;
-
-            if ind<length(rvals)
-                nvals(ind)=nvals(ind)+1; %just count collisions
-                evals(ind)=evals(ind)+de; %count energy
+        skip=isempty(A);
+        if skip
+            continue
+        end
+        
+        [aa,n]=sscanf(A,'%f %f %f %f %f %f %f %s');
+        
+        if n==8
+            try
+                de=lastLine(7)-aa(7);
+            catch
+                de=0;
             end
             
-            count=count+1;
+            if ~isempty(regexp(A,'MMA')) %Only look at collisions in PMMA or MMA
+                xv=[xv aa(1)];
+                yv=[yv aa(2)];
+                r=(aa(1)^2+aa(2)^2)^(0.5);
+                ind=round(r/dr)+1;
+                
+                if ind<length(rvals)
+                    nvals(ind)=nvals(ind)+1; %just count collisions
+                    evals(ind)=evals(ind)+de; %count energy
+                end
+                
+                count=count+1;
+            end
+            
+        end
+        
+        lastLine=aa;
+        
+        if count>0 & ~mod(count,1000)
+            fprintf('%d collisions \n',count);
         end
         
     end
-    
-    lastLine=aa;
-    
-    if count>0 & ~mod(count,1000)
-        fprintf('%d collisions \n',count);
-    end
-    
+    toc
+    fclose(fID);
 end
-toc
-fclose(fID);
 
 r2=pi.*rvals.^2; %circle area
 aa=r2-[0 r2(1:end-1)];  %right area
@@ -96,7 +118,9 @@ r=sum(evals(inds(end)+1:end)); %approximation of the back-scattered energy
 et=r/f; %ratio of backscattered energy to forward energy
 
 fitfn=@(p,x) psf3(p,x);
-beta0=[1 4  1.1 et .1]; %parameters are [log10(alpha) log10(beta) log10(gamma) total backscattered ratio nu]
+beta0=config.beta0;
+beta0(1)=log10(forwardRange/3); %parameters are [log10(alpha) log10(beta) log10(gamma) total backscattered ratio nu]
+beta0(4)=et;  
 mask=[1 1 1 0 1];
 
 logKeep=ones(1,length(rvals));
@@ -133,6 +157,9 @@ psf.beta0=beta0;
 psf.mask=mask;
 psf.x=x;
 psf.y=y;
+psf.rvals=rvals;
+psf.evals=evals;
+psf.edensity=edensity;
 
 descr=input('Enter a description, e.g., Si30kV_500 \n','s');
 psf.descr=descr;
@@ -144,3 +171,9 @@ save(outputFileName,'psf');
 
 end
 
+% Apply a default.
+function s=def(s,f,v)
+if(~isfield(s,f))
+    s=setfield(s,f,v);
+end
+end
