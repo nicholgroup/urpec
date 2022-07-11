6a2071dd18d47c6eca0ae4f04e1af48e616e47d6
John M. Nichol <jmnichol@gmail.com>
2022-07-11 13:40:48 -0400

    Update checkConvex.m

--- checkConvex.m
+++ checkConvex.m
@@ -1,4 +1,4 @@
-function isConvex = checkConvex(px,py)
+function [isConvex,v] = checkConvex(px,py)
 % Given a set of points determine if they form a convex polygon
 % Inputs 
 %  px, py: x and y coordinates of the vertices for a given polygon
@@ -26,24 +26,33 @@ end
 v1 = [px(1) - px(end), py(1) - py(end)];
 v2 = [px(2) - px(1), py(2) - py(1)];
 signPoly = sign(det([v1; v2]));
+v(1)=signPoly;
 
 % check subsequent vertices
 for k = 2:numPoints-1
     v1 = v2;
     v2 = [px(k+1) - px(k), py(k+1) - py(k)]; 
     curr_signPoly = sign(det([v1; v2]));
+    v(k)=curr_signPoly;
     % check that the signs match
-    if not (isequal(curr_signPoly, signPoly))
-        isConvex = false;
-        return
-    end
+%     if not (isequal(curr_signPoly, signPoly))
+%         isConvex = false;
+%         return
+%     end
 end
 % check the last vectors
 v1 = v2;
 v2 = [px(1) - px(end), py(1) - py(end)];
 curr_signPoly = sign(det([v1; v2]));
-if not (isequal(curr_signPoly, signPoly))
-    isConvex = false;
+v(end+1)=curr_signPoly;
+% if not (isequal(curr_signPoly, signPoly))
+%     isConvex = false;
+% else
+%     isConvex = true;
+% end
+
+if any(v(2:end)~=v(1))
+    isConvex=false;
 else
-    isConvex = true;
+    isConvex=true;
 end
