123a7bae90aff08985a4a92d8e343c9c152798b8
John M. Nichol <jmnichol@gmail.com>
2022-09-12 13:55:18 -0400

    Update patternText.m
    Add simplify_polygon as a local function

--- patternText.m
+++ patternText.m
@@ -298,3 +298,19 @@ for i=1:length(BB)
 end
 end
 
+
+function P = simplify_polygon(P)
+% function P = simplify_polygon(P)
+% Removes unnecessary vertices from a polygon
+% Shamelessly stolen from the website below.
+% https://blogs.mathworks.com/steve/2012/08/28/wrapping-up-the-analysis-of-cody-solutions/
+  try
+  diag(sum(abs(diff(P)),2)) \ diff(P);
+  P(any(ans - circshift(ans,1),2),:);
+  P = vertcat(ans, ans(1,:));
+  end
+  
+end
+
+
+
